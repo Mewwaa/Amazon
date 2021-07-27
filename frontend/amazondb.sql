8ea51f371e0349c4cd941bc263cf1d90ed867aa4
Ewa <79149134+Mewwaa@users.noreply.github.com>
2021-07-27 23:41:40 +0200

    Delete amazondb.sql

--- frontend/amazondb.sql
+++
@@ -1,3 +0,0 @@
-create database Amazon;
-create table Users (`user_id` int AUTO_INCREMENT primary key , `name` varchar(45), `surname` varchar(45), `login` varchar(100), `password` int);
-create table Books (`book_id` int AUTO_INCREMENT primary key, `title` varchar(45), `author` varchar(45), `quantity` int);
