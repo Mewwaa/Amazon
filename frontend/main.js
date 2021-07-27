6bb85bf91b7ea69d06939c5ac21691e09076dbbf
Ewa <79149134+Mewwaa@users.noreply.github.com>
2021-07-27 23:44:06 +0200

    aktualizacja html i innych plików z frontend

---
+++ frontend/main.js
@@ -0,0 +1,11 @@
+import React from 'react'; // import potrzebnych bibliotek
+import ReactDOM from 'react-dom';  // import potrzebnych bibliotek
+
+import AmazonMain from './1/AmazonMain.jsx';
+
+
+
+ReactDOM.render(<div>
+    <AmazonMain/>
+</div>, document.getElementById('app'));
+
